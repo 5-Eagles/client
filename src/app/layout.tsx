2ac5026a07fbfeb9a6d9d9afe52c1b09089a5fbe
Raehyeon Jeong <raehy19@gmail.com>
2025-01-11 16:17:18 +0900

    feat : add basic email login with supabase

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import { Geist } from 'next/font/google';
+import './globals.css';
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: '5-Eagles',
+  description: '',
+};
+
+const geistSans = Geist({
+  display: 'swap',
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' className={geistSans.className} suppressHydrationWarning>
+      <body className='bg-background text-foreground'>
+        <main className='min-h-screen flex flex-col items-center'>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
