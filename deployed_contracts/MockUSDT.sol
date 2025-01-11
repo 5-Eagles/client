740b84ab7d00c0e067cbab54f4da07f6cf6ed4cf
Raehyeon Jeong <raehy19@gmail.com>
2025-01-12 02:30:00 +0900

    feat : add deployed contract solidity code. MockUSDT, ServiceProvider

---
+++ deployed_contracts/MockUSDT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockUSDT is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("MockUSDT", "mUSDT") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        require(to != address(0), "Cannot mint to the zero address");
+        require(amount > 0, "Amount must be greater than zero");
+        _mint(to, amount);
+    }
+}
+
+
+// deployed on eth-sepolia, contract address: 0x680f3BE6C552e53BfF662522f95b062D8B4B4F54
